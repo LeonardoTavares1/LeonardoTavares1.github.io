4c70d1e3b9e6e6e7d84dafd27839ad7f16654238
LeonardoTavares1 <leonardao1234@gmail.com>
2022-05-30 19:26:57 -0300

    poxa_bruno1

--- src/views/App.js
+++ src/views/App.js
@@ -1,17 +1,10 @@
-//Com a extensao Simple React Snippets instalada, utlize o atalho imr para importar o react automaticamente e sfc para criar o componente
-
 import React from 'react'; 
-/*
-No caso abaixo vemos como importar diferentes componentes React vindos de um mesmo arquivo
-*/
-import Tarefa, { Imagem } from '../components/tarefa';
+import Contato from '../components/Contato';
 
-
-//Neste arquvio esta a demonstração da ultilização de varios components react dentro de uma mesma pagina.
 const App = () => {
     return ( 
         <>
-           <Tarefa />
+            <Contato />
         </>
      );
 }
