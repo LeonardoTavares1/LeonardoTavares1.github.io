4caf6aa9c3a96c9df22cafdca8e17caecae731b5
LeonardoTavares1 <leonardao1234@gmail.com>
2022-06-14 19:08:06 -0300

    Cadastro

--- src/components/Contato.js
+++ src/components/Contato.js
@@ -6,11 +6,13 @@ const Contato = () => {
         <>
             <div className='main'>
                     <form className='meu_form'>
-                        <p class="sexo" >Entre em contato</p>
-                        <input type="text" placeholder="Seu nome" />
+                        <p class="sexo" >Cadastro</p>
+                        <input type="text" placeholder="Digite seu nome" />
                         <input type="tel" placeholder="Número de celular" />
                         <input type="email" placeholder="Digite seu e-mail" />
-                        <textarea placeholder="Deixe sua opnião"></textarea>
+                        <input type="password" placeholder='Criar uma senha' />
+                        <input type="password" placeholder='Repetir sua senha' />
+                        <br />
                         <input type="submit" class="enviar" value="Enviar"  />
                     </form>
              
